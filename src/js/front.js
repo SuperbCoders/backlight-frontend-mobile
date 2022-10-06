0299c742300af24811e7f3da17005f4c4dd1e2b5
Bekzod <bekzod101001@gmail.com>
2022-10-06 22:16:06 +0500

    updates

--- src/js/front.js
+++ src/js/front.js
@@ -1,181 +1,227 @@
-const server = `${window.location.protocol}//${window.location.host}`
+const server = 'https://blacklight.ru';
+const groupBy = (items, key) => items.reduce(
+  (result, item) => ({
+    ...result,
+    [item[key]]: [
+      ...(result[item[key]] || []),
+      item,
+    ],
+  }),
+  {},
+);
 new Vue({
-    el: '#cases',
-    data() {
-        return {
-            info: {}
-        };
-    },
-    mounted() {
-        axios
-            .get(`${server}/api/cases/?format=json`)
-            .then(response => {
-                this.info = response.data
-                this.info = this.info.map((elem) => elem.pdf.replace('http', 'https'))
-            })
-    },
-});
+  el: '#cases',
+  data () {
+    return {
+      activeYear: null,
+      items: []
+    };
+  },
+  computed: {
+    computedYears () {
+      const items = this.items.map((elem) => {
+        elem.pdf.replace('http', 'https')
+        elem.year = elem.date_at.substr(0, 4)
+        return elem;
+      });
 
-
-new Vue({
-    el: '#awards',
-    data() {
-        return {
-            info: {}
-        };
-    },
-    mounted() {
-        axios
-            .get(`${server}/api/rewards/?format=json`)
-            .then(response => (this.info = response.data))
-    },
-    methods: {
-        openMore: function (index) {
-            $('.awards-full-item .window').eq(index).addClass('active');
-        },
-    }
-});
-
-new Vue({
-    el: '#awards-full',
-    data() {
-        return {
-            info: {}
-        };
-    },
-    mounted() {
-        axios
-            .get(`${server}/api/rewards/?format=json`)
-            .then(response => (this.info = response.data))
-    },
-    methods: {
-        closeMore: function () {
-            $('.window-awards-open').removeClass('active');
-        },
-    }
-});
-
-
-new Vue({
-    el: '#note',
-    data() {
-        return {
-            info: {}
-        };
-    },
-    mounted() {
-        axios
-            .get(`${server}/api/text/main_about/?format=json`)
-            .then(response => (this.info = response.data))
-    }
-});
-
-
-new Vue({
-    el: '#presentation',
-    data() {
-        return {
-            info: {}
-        };
-    },
-    mounted() {
-        axios
-            .get(`${server}/api/file/presentation/?format=json`)
-            .then(response => {
-                this.info = response.data
-                this.info.file_mobile += '#toolbar=0'
-            })
-    },
-});
-
-new Vue({
-    el: '#team',
-    data() {
-        return {
-            info: {}
-        };
-    },
-    mounted() {
-        axios
-            .get(`${server}/api/employees/?format=json`)
-            .then(response => (this.info = response.data.filter(item => item.status == true)))
-    },
-    methods: {
-        openMore: function (index) {
-            $('.team-full-item .window').eq(index).addClass('active');
-        },
-    },
-    filters: {
-        nameOnly (value) {
-            return value.split(' ')[0];
+      let years = [];
+      items.forEach(item => {
+        let year = years.find(year => year.value === item.year)
+        if(year) {
+          year.items.push(item);
+        } else {
+          years.push({
+            value: item.year,
+            items: []
+          })
         }
+      });
 
+
+      return years.sort((a, b) => parseInt(b.value) - parseInt(a.value));
     }
+  },
+  methods: {
+    toggleActiveYear(year) {
+      if(this.activeYear === year) this.activeYear = null
+
+      this.activeYear = year;
+    },
+    loadItems (year) {
+      // fancy();
+      axios.get(`${server}/api/cases/${year}/?format=json`).then((response) => {
+        this.cases = response.data;
+      });
+    },
+  },
+  mounted () {
+    axios
+      .get(`${ server }/api/cases/?format=json`)
+      .then(response => {
+        this.items = response.data
+      })
+  },
 });
 
 new Vue({
-    el: '#team-full',
-    data() {
-        return {
-            info: {}
-        };
+  el: '#awards',
+  data () {
+    return {
+      info: {}
+    };
+  },
+  mounted () {
+    axios
+      .get(`${ server }/api/rewards/?format=json`)
+      .then(response => (this.info = response.data))
+  },
+  methods: {
+    openMore: function (index) {
+      $('.awards-full-item .window').eq(index).addClass('active');
     },
-    mounted() {
-        axios
-            // .get('test.json')
-            .get(`${server}/api/employees/?format=json`)
-            .then(response => (this.info = response.data))
+  }
+});
+
+new Vue({
+  el: '#awards-full',
+  data () {
+    return {
+      info: {}
+    };
+  },
+  mounted () {
+    axios
+      .get(`${ server }/api/rewards/?format=json`)
+      .then(response => (this.info = response.data))
+  },
+  methods: {
+    closeMore: function () {
+      $('.window-awards-open').removeClass('active');
     },
-    methods: {
-        closeMore: function () {
-            $('.window-team-open').removeClass('active');
-        },
+  }
+});
+
+new Vue({
+  el: '#note',
+  data () {
+    return {
+      info: {}
+    };
+  },
+  mounted () {
+    axios
+      .get(`${ server }/api/text/main_about/?format=json`)
+      .then(response => (this.info = response.data))
+  }
+});
+
+new Vue({
+  el: '#presentation',
+  data () {
+    return {
+      info: {}
+    };
+  },
+  mounted () {
+    axios
+      .get(`${ server }/api/file/presentation/?format=json`)
+      .then(response => {
+        this.info = response.data
+        this.info.file_mobile += '#toolbar=0'
+      })
+  },
+});
+
+new Vue({
+  el: '#team',
+  data () {
+    return {
+      info: {}
+    };
+  },
+  mounted () {
+    axios
+      .get(`${ server }/api/employees/?format=json`)
+      .then(response => (this.info = response.data.filter(item => item.status == true)))
+  },
+  methods: {
+    openMore: function (index) {
+      $('.team-full-item .window').eq(index).addClass('active');
+    },
+  },
+  filters: {
+    nameOnly (value) {
+      return value.split(' ')[0];
     }
-});
 
-
-new Vue({
-    el: '#phone',
-    data() {
-        return {
-            info: {}
-        };
-    },
-    mounted() {
-        axios
-            .get(`${server}/api/contacts/?format=json`)
-            .then(response => (this.info = response.data))
-    },
+  }
 });
 
 new Vue({
-    el: '#modal-contacts',
-    data() {
-        return {
-            contacts: {},
-            socials: []
-        };
+  el: '#team-full',
+  data () {
+    return {
+      info: {}
+    };
+  },
+  mounted () {
+    axios
+      // .get('test.json')
+      .get(`${ server }/api/employees/?format=json`)
+      .then(response => (this.info = response.data))
+  },
+  methods: {
+    closeMore: function () {
+      $('.window-team-open').removeClass('active');
     },
-    mounted() {
-        axios
-            .get(`${server}/api/contacts/?format=json`)
-            .then(response => {this.contacts = response.data})
-        axios
-            .get(`${server}/api/urls/?format=json`)
-            .then((response) => (this.socials = response.data));
-    },
+  }
 });
 
 new Vue({
-    el: '#footer',
-    data() {
-        return {
-            socials: []
-        };
-    },
-    mounted() {
-        axios
-            .get(`${server}/api/urls/?format=json`)
-            .then((response) => (this.socials = response.data));
-    },
+  el: '#phone',
+  data () {
+    return {
+      info: {}
+    };
+  },
+  mounted () {
+    axios
+      .get(`${ server }/api/contacts/?format=json`)
+      .then(response => (this.info = response.data))
+  },
+});
+
+new Vue({
+  el: '#modal-contacts',
+  data () {
+    return {
+      contacts: {},
+      socials: []
+    };
+  },
+  mounted () {
+    axios
+      .get(`${ server }/api/contacts/?format=json`)
+      .then(response => {
+        this.contacts = response.data
+      })
+    axios
+      .get(`${ server }/api/urls/?format=json`)
+      .then((response) => (this.socials = response.data));
+  },
+});
+
+new Vue({
+  el: '#footer',
+  data () {
+    return {
+      socials: []
+    };
+  },
+  mounted () {
+    axios
+      .get(`${ server }/api/urls/?format=json`)
+      .then((response) => (this.socials = response.data));
+  },
 });
